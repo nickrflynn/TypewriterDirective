73c77c0c5ed6f9fa26abcad38d38dcc3739cbe54
Nick Flynn <nick.flynn@healthcatalyst.com>
2017-10-08 17:29:01 -0600

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <h1>
-      Welcome to {{title}}!!
-    </h1>
-  `,
-  styles: []
+    selector: 'app-root',
+    template: 
+        `<h1 typewriter [toType]="codeNames" startDelay="1000" delay="2500" includePipe="true" fastDelete="true"></h1>`
+  ,
+    styles: []
 })
 export class AppComponent {
-  title = 'app';
+    codeNames = [
+        'interdimensional-cable',
+        'toffee-salinization',
+        'phenomenal-footnote',
+        'scriptalicious-bootstrapping',
+        'llamacorn-lovehug',
+        'princely-quest',
+        'promise-rectification',
+        'asynchronous-synchronization',
+        'rainbow-tsunami',
+        'pathological-kerning',
+        'overlyexplosive-poprocks',
+        'cookie-liberation',
+        'rocket-zambonimation',
+        'permanent-internship',
+        'accidental-haiku',
+        'emotional-waffles',
+        'emoji-clairvoyance',
+        'monochromatic-rainbow',
+        'debilitating-awesomeness',
+        'tesseract-giftwrapping',
+        'bubblewrap-cape',
+        'jiggling-armfat',
+        'vegetable-reanimation',
+        'lethal-stutter',
+        'radioactive-gargle',
+        'badger-enumeration',
+        'finnish-disembarkation',
+        'instantaneous-browserification',
+        'meticulous-riffleshuffle',
+        'pylon-requirement',
+    ]
 }
